1605c4f3831423a4d4200fbc518a73f62719b899
Jithu <jithukrishnaju@gmail.com>
2026-06-13 15:07:37 +0530

    initial commit

---
+++ src/main/java/com/pm/pati/PatiApplication.java
@@ -0,0 +1,13 @@
+package com.pm.pati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/pati/PatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.pati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
