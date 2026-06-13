71da64ee8db68cf8cab67855a48dd3f8df1c449c
Jithu <jithukrishnaju@gmail.com>
2026-06-13 22:09:04 +0530

    gradle build time check

--- src/main/java/com/pm/pati/controller/TestController.java
+++ src/main/java/com/pm/pati/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "testing this by me again";
+        return "testing this by me again, gradle build check again";
     }
 }
