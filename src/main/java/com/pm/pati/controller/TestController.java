6f7a9934065526e75926f0d85157a22cba757463
Jithu <jithukrishnaju@gmail.com>
2026-06-13 21:59:04 +0530

    test controller

--- src/main/java/com/pm/pati/controller/TestController.java
+++ src/main/java/com/pm/pati/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "testing this";
+        return "testing this by me";
     }
 }
